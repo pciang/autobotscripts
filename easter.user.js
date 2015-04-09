d049c071724e0e32bcd76a9995d6af6872c64acc
Peter <peterch1ang@yahoo.com>
2015-04-09 10:10:47 +0800

    update

--- easter.user.js
+++ easter.user.js
@@ -13,6 +13,18 @@
 // ==/UserScript==
 
 document.addEventListener('DOMContentLoaded', function (arg) {
+	var easterSettings;
+	var storageKey = 'easterbot_settings';
+
+	if(localStorage.getItem(storageKey)) {
+		easterSettings = JSON.parse(localStorage.getItem(storageKey));
+	} else {
+		easterSettings = {
+			activate: true,
+			useNewCharm: false
+		};
+	}
+
 	var App_Container = document.getElementById('hgAppContainer');
 
 	var Settings_Box = document.createElement('div');
@@ -23,7 +35,68 @@ document.addEventListener('DOMContentLoaded', function (arg) {
 			+ ' border-right: 1px inset #830300; border-top: 3px double #830300;'
 			+ ' border-bottom: 3px double #830300; padding: 5px 0 5px;');
 
-	Settings_Box.appendChild(document.createTextNode('MouseHunt EasterBot loaded!'));
+	// Settings_Box content
+	(function () {
+		var div;
+		var temp;
+
+		/* Line 1 */
+		div = document.createElement('div');
+		div.appendChild(document.createTextNode('MouseHunt EasterBot loaded!'));
+		Settings_Box.appendChild(div);
+
+
+		/* Line 2 */
+		div = document.createElement('div');
+
+		temp = document.createElement('b');
+		temp.appendChild(document.createTextNode('Activate bot: '));
+		div.appendChild(temp);
+		
+		temp = document.createElement('input');
+		temp.setAttribute('type', 'checkbox');
+		temp.checked = easterSettings.activate;
+		temp.onchange = function (e) {
+			easterSettings.activate = this.checked;
+		};
+		div.appendChild(temp);
+
+		Settings_Box.appendChild(div);
+
+		/* Line 3 */
+		div = document.createElement('div');
+
+		temp = document.createElement('b');
+		temp.appendChild(document.createTextNode('!!Not working yet!!: '));
+		div.appendChild(temp);
+
+		temp = document.createElement('input');
+		temp.setAttribute('type', 'checkbox');
+		temp.checked = easterSettings.useNewCharm;
+		temp.onchange = function (e) {
+			easterSettings.useNewCharm = this.checked;
+		};
+		div.appendChild(temp);
+
+		temp = document.createElement('i');
+		temp.appendChild(document.createTextNode('(Enabling this will allow swapping between Eggstra Charge Charm & Eggstra Charm!)'));
+		div.appendChild(temp);
+
+		Settings_Box.appendChild(div);
+
+		/* Line 4 */
+		div = document.createElement('div');
+
+		temp = document.createElement('button');
+		temp.appendChild(document.createTextNode('Save'));
+		temp.onclick = function (e) {
+			console.log(JSON.stringify(easterSettings));
+			localStorage.setItem(storageKey, JSON.stringify(easterSettings));
+		};
+		div.appendChild(temp);
+
+		Settings_Box.appendChild(div);
+	})();
 
 	var Charge_Qty = document.getElementsByClassName("chargeQuantity")[0];
 
@@ -35,28 +108,33 @@ document.addEventListener('DOMContentLoaded', function (arg) {
 	var loadedCharm = '';
 	var eggchargeCharm = 'egg_charge_trinket';
 	var eggstraCharm = 'eggstra_trinket';
+	var newCharm = 'eggstra_charge_trinket';
 
 	// Initialization
-	function () {
+	(function () {
 		if(chargeQty < 20) {
 			hg.utils.TrapControl.setTrinket(eggchargeCharm).go();
 		} else if(chargeQty == 20) {
 			hg.utils.TrapControl.setTrinket(eggstraCharm).go();
 		}
-	}();
+	})();
 
 	function listen() {
 		chargeQty = parseInt(Charge_Qty.textContent.substring(0, Charge_Qty.textContent.indexOf('/')));
 		
-		// Workaround for arming charms
-		if(chargeQty < 18 && loadedCharm !== eggchargeCharm) {
-			hg.utils.TrapControl.setTrinket(loadedCharm = eggchargeCharm).go();
-		} else if(chargeQty == 20 && loadedCharm !== eggstraCharm) {
-			hg.utils.TrapControl.setTrinket(loadedCharm = eggstraCharm).go();
+		// If bot is allowed to activate
+		if(easterSettings.activate) {
+			// Workaround for arming charms
+			if(chargeQty < 18 && loadedCharm !== eggchargeCharm) {
+				hg.utils.TrapControl.setTrinket(loadedCharm = eggchargeCharm).go();
+			} else if(chargeQty == 20 && loadedCharm !== eggstraCharm) {
+				hg.utils.TrapControl.setTrinket(loadedCharm = eggstraCharm).go();
+			}
 		}
 
 		setTimeout(listen, 10000);
 	}
 
+	alert('EasterBot script successfully loaded!');
 	listen();
 });
